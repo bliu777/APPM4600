5189ac8beddf83234d548d61d2e9d3b2f9aefe97
Byron Liu <byli4778@colorado.edu>
2023-10-12 14:26:09 -0600

    Lab 7

---
+++ Labs/Lab 7/interp.py
@@ -0,0 +1,135 @@
+import numpy as np
+import numpy.linalg as la
+import matplotlib.pyplot as plt
+
+def driver():
+
+
+    f = lambda x: 1/(1 + (10 * x)**2)
+
+    ''' degree'''
+    N = 4 
+    ''' interval'''
+    a = -1
+    b = 1
+       
+   
+    ''' create equispaced interpolation nodes'''
+    xint = np.linspace(a,b,N+1)
+    
+    ''' create interpolation data'''
+    yint = f(xint)
+    
+    ''' create points for evaluating the Lagrange interpolating polynomial'''
+    Neval = 1000
+    xeval = np.linspace(a,b,Neval+1)
+    yeval_m = np.zeros(Neval+1)
+    yeval_l = np.zeros(Neval+1)
+    yeval_dd = np.zeros(Neval+1)
+  
+    '''Initialize and populate the first columns of the 
+     divided difference matrix. We will pass the x vector'''
+    y = np.zeros( (N+1, N+1) )
+     
+    for j in range(N+1):
+       y[j][0]  = yint[j]
+
+    y = dividedDiffTable(xint, y, N+1)
+    ''' evaluate lagrange poly '''
+    for kk in range(Neval+1):
+       #yeval_m[kk] = 
+       monom_eval(xeval[kk],xint,yint,N)
+       yeval_l[kk] = eval_lagrange(xeval[kk],xint,yint,N)
+       yeval_dd[kk] = evalDDpoly(xeval[kk],xint,y,N)
+          
+
+    
+
+
+    ''' create vector with exact values'''
+    fex = f(xeval)
+       
+
+    plt.figure()    
+    plt.plot(xeval,fex,'ro-')
+    plt.plot(xeval,yeval_l,'bs--') 
+    plt.plot(xeval,yeval_dd,'c.--')
+    plt.legend()
+
+    plt.figure() 
+    err_l = abs(yeval_l-fex)
+    err_dd = abs(yeval_dd-fex)
+    plt.semilogy(xeval,err_l,'ro--',label='lagrange')
+    plt.semilogy(xeval,err_dd,'bs--',label='Newton DD')
+    plt.legend()
+    plt.show()
+
+
+def vander(xint, N):
+    vand = np.zeros((N+1,N+1))
+
+    for i in range(N+1):
+        for j in range(N+1):
+            vand[i][j] = xint[j]**i
+
+    return vand
+
+
+def monom_eval(xeval,xint,yint,N):
+    vand = vander(xint, N)
+    vand_inv = la.inv(vand)
+    coeffs = np.matmul(vand_inv, yint)
+
+    yeval = np.zeros(1000)
+    for i in range(1000):
+        for j in range(N + 1):
+            yeval[i] += coeffs[j] * (xeval[i]**j)
+
+    return yeval
+
+
+def eval_lagrange(xeval,xint,yint,N):
+    lj = np.ones(N+1)
+    
+    for count in range(N+1):
+       for jj in range(N+1):
+           if (jj != count):
+              lj[count] = lj[count]*(xeval - xint[jj])/(xint[count]-xint[jj])
+
+    yeval = 0.
+    
+    for jj in range(N+1):
+       yeval = yeval + yint[jj]*lj[jj]
+  
+    return(yeval)
+  
+    
+
+
+''' create divided difference matrix'''
+def dividedDiffTable(x, y, n):
+ 
+    for i in range(1, n):
+        for j in range(n - i):
+            y[j][i] = ((y[j][i - 1] - y[j + 1][i - 1]) /
+                                     (x[j] - x[i + j]));
+    return y;
+    
+def evalDDpoly(xval, xint,y,N):
+    ''' evaluate the polynomial terms'''
+    ptmp = np.zeros(N+1)
+    
+    ptmp[0] = 1.
+    for j in range(N):
+      ptmp[j+1] = ptmp[j]*(xval-xint[j])
+     
+    '''evaluate the divided difference polynomial'''
+    yeval = 0.
+    for j in range(N+1):
+       yeval = yeval + y[0][j]*ptmp[j]  
+
+    return yeval
+
+       
+
+driver()        
